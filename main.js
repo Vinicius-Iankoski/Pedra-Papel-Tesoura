f0cbf2bf8b56cf1ba33ebe14c0c4eb34738ed093
Vinicius-Iankoski <vine.iankoski@gmail.com>
2024-03-08 15:14:58 -0300

    criacao das funcoes principais

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// funcao para mudar o display da tela inicial
 function abrirJogo() {
     let display = document.getElementById("main-content").style.display;
     if (display == "none") {
@@ -8,4 +9,14 @@ function abrirJogo() {
         document.getElementById("main-content").style.display = "none";
         document.getElementById("wrapper-display").style.display = "grid";
     }
+};
+
+// funcao para fazer a animação CSS quando o jogador clicar em algum div
+function transicaoDeEscolha() {
+
+};
+
+// funcao para guardar a escolha do jogador
+function guardarEscolha() {
+
 }
